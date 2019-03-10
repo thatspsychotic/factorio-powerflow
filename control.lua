14a171932402c205babaf8ff2bab91d5a503e954
thatspsychotic <thatspsychotic@gmail.com>
2019-03-10 19:04:51 -0400

    Early test-case, power pole neighbors IDs announced on mouse-over

---
+++ control.lua
@@ -0,0 +1,29 @@
+
+--To Implement--
+
+
+
+
+
+local function show_selected_pole_neighbors(event)
+	local item = game.get_player(event.player_index).selected
+	local neighbors
+	
+	if(item ~= nil) then
+		if(item.type == "electric-pole") then
+			game.print("Electric pole has neighbors:")
+			for _, neighbor in pairs(item.neighbours["copper"]) do
+				game.print(neighbor.unit_number)
+			end
+		end
+	end
+end
+
+
+--[[ Whole thing is event handlers at this stage]]--
+
+local e=defines.events
+local remove_events = {e.on_player_mined_entity, e.on_robot_pre_mined, e.on_entity_died}
+local add_events = {e.on_built_entity, e.on_robot_built_entity}
+
+script.on_event(e.on_selected_entity_changed, show_selected_pole_neighbors)
