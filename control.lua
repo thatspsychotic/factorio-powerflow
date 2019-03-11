c1ed4275a89ec782a42e0ac94663140cb2a101a8
thatspsychotic <thatspsychotic@gmail.com>
2019-03-11 07:42:06 -0400

    small incremental update, pole data structure work

--- control.lua
+++ control.lua
@@ -1,10 +1,34 @@
 
 --To Implement--
 
-function initialize()
-	
-end
+pole_data = {
+	-- vanilla & similar
+	[{"big-electric-pole"}] = {
+		["x"] = 0.025,
+		["rating"] = 750,
+	},
+	[{"medium-electric-pole"}] = {
+		["x"] = 0.15,
+		["rating"] = 200,
+	},	
+	[{"small-electric-pole"}] = {
+		["x"] = 0.4,
+		["rating"] = 50,
+	},	
+	[{"substation"}] = {
+		["x"] = 0.2,
+		["rating"] = 300,
+	},	
+}
 
+local function find_all_poles()
+	all_poles = {}
+	for _, surface in pairs(game.surfaces) do
+		for _, pole in pairs(surface.find_entities_filtered{type="electric-pole"}) do
+			all_poles[pole.unit_number] = pole
+		end
+	end
+end
 
 
 local function show_selected_pole_neighbors(event)
@@ -21,6 +45,11 @@ local function show_selected_pole_neighbors(event)
 	end
 end
 
+local function initialize()
+	print("Total number of electric poles on map")
+	print(table.getn(game.surfaces[1].find_entities_filtered(type="electric-pole)))
+	game.print("It worked!")
+end
 
 --[[ Whole thing is event handlers at this stage]]--
 
@@ -29,4 +58,4 @@ local remove_events = {e.on_player_mined_entity, e.on_robot_pre_mined, e.on_enti
 local add_events = {e.on_built_entity, e.on_robot_built_entity}
 
 script.on_event(e.on_selected_entity_changed, show_selected_pole_neighbors)
-script.on_init(e.on_init, initialize)
+script.on_init(initialize)
